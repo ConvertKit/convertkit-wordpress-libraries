36eb36bc303e35c480f74de1cca28a10df2ed338
Tim Carr <tim@n7studios.co.uk>
2024-01-12 13:31:57 +0000

    Coding standards

--- src/class-convertkit-api.php
+++ src/class-convertkit-api.php
@@ -1496,15 +1496,15 @@ class ConvertKit_API {
 
 		// Define convertkit JS object.
 		$js_convertkit_object = array(
-			'ajaxurl'       => admin_url( 'admin-ajax.php' ),
-			'nonce'         => wp_create_nonce( 'convertkit' ),
+			'ajaxurl' => admin_url( 'admin-ajax.php' ),
+			'nonce'   => wp_create_nonce( 'convertkit' ),
 		);
 
 		// Inject JS for subscriber forms to work.
 		// wp_enqueue_script() isn't called when we load a Landing Page, so we can't use it.
 		// phpcs:disable WordPress.WP.EnqueuedResources
 		$script  = "<script type='text/javascript' src='" . $this->plugin_url . 'resources/frontend/js/convertkit.js?ver=' . $this->plugin_version . "'></script>";
-		$script .= "<script type='text/javascript'>/* <![CDATA[ */var convertkit = " . wp_json_encode( $js_convertkit_object ) . ";/* ]]> */</script>";
+		$script .= "<script type='text/javascript'>/* <![CDATA[ */var convertkit = " . wp_json_encode( $js_convertkit_object ) . ';/* ]]> */</script>';
 		// phpcs:enable
 
 		$body = str_replace( '</head>', '</head>' . $script, $body );
